4da23025a25a10a468d17bf237c16d908bac7c9d
Jared Harbison <jared.harbison@gmail.com>
2019-01-13 20:46:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -8,5 +8,13 @@ def alphabetize(arr)
 end
 
 
+def alphabetize(arr)
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
+end
 
 

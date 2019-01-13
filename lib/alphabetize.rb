8deb70bb568c1f3a683e9550294c5687861d7079
Jared Harbison <jared.harbison@gmail.com>
2019-01-13 20:52:22 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,15 +1,5 @@
 def alphabetize(arr)
-  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".scan /\w/
-  arr.sort_by do |word|
-    word.split('').collect do |letter|
-      esperanto_alphabet.index(letter)
-    end
-  end
-end
-
-
-def alphabetize(arr)
-  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".scan(/./) 
   arr.sort_by do |word|
     word.split('').collect do |letter|
       esperanto_alphabet.index(letter)
